402d22f4860196c879ded438d46e2573a6603387
Mina Osma <minaosaama901@gmail.com>
2023-11-05 02:04:34 +0200

    2:04

--- src/src_cfg.mk
+++ src/src_cfg.mk
@@ -1,4 +1,11 @@
-cc := avr-gcc
+cc := avr
+f := -gcc
 un := $(shell whoami)
 mcu:= -mmcu=atmega32
 lib_path := ../lib/libMDIO.a 
+elf_to_hex := avr-objcopy -O ihex
+hex := snake_effect.hex
+build_dir := ./build
+bin := build/binaryReport.txt
+elf_size := avr-size -t build/snake_effect.elf
+hex_size := avr-size -t build/snake_effect.hex
